324983498593a7804b6937f65207b9c8a8255b2e
María del Pilar Alegre Ramos <malegrera@uoc.edu>
2022-04-27 14:39:47 +0200

    cambios

---
+++ datos.py
@@ -0,0 +1,16 @@
+from flask import json
+#import json json.dumps()
+
+def recibedatos():
+    pythonDictionary = {'1':{'name':'Bob', 'age':44, 'isEmployed':True},'2':{'name':'Peter', 'age':55, 'isEmployed':False},'3':{'name':'John', 'age':33, 'isEmployed':True}}
+    dictionaryToJson = json.jsonify(pythonDictionary)
+    return dictionaryToJson
+
+def recibemostrardatos(datos):
+   if (datos=='empleados'):
+       empleados = {'1':{'name':'Bob', 'age':44, 'isEmployed':True},'2':{'name':'Peter', 'age':55, 'isEmployed':False},'3':{'name':'John', 'age':33, 'isEmployed':True}}
+       a=empleados
+   else:
+      alumnos={'1':{'Nombre':'Pedro', 'Edad':23, 'Nota':7},'2':{'Nombre':'Francisco', 'Edad':44, 'Nota':8},'3':{'Nombre':'Alberto', 'Edad':33, 'Nota':10},'4':{'Nombre':'Alejandro', 'Edad':23, 'Nota':9},'5':{'Nombre':'Emilio', 'Edad':34, 'Nota':6.5}}
+      a=alumnos
+   return a
